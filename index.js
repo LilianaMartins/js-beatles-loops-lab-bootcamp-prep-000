d386d0836babaae1780895128affb202826c9e5a
lilianasrmartins@gmail.com <lilianasrmartins@gmail.com>
2018-07-29 17:52:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
 function theBeatlesPlay(musicians,instruments) {
   const myArray = [];
 
-  for (let i = 0; i <= musicians.length; i++) {
+  for (let i = 0; i < musicians.length; i++) {
   myArray.push(`${musicians[i]} plays ${instruments[i]}`)
 }
 
